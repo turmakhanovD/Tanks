250ec252ffb01c5f265540207545124542c9cbf6
Islam <denizzz9998882@gmail.com>
2019-02-08 23:02:18 +0600

    firs

---
+++ WOT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MyClassLibrary;
+
+namespace WOT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank t34 = new Tank("T-34");
+            Tank pantera = new Tank("Pantera");
+            Tank winnerTank = t34 ^ pantera;
+            winnerTank.GetInfo(winnerTank);
+            Console.ReadLine();
+
+        }
+    }
+}
