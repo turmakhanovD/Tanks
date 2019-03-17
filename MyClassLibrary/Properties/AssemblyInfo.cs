1e741d13aa10563af5399e8c24995e83ea0f6c43
turmakhanovD <46023441+turmakhanovD@users.noreply.github.com>
2019-03-17 16:39:38 +0600

    Add files via upload

---
+++ MyClassLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("MyClassLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyClassLibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2f6e0390-9929-4129-a910-22561ac88913")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyClassLibrary/Tank.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary
+{
+    public class Tank
+    {
+        private string Name { get; set; }
+        private int Armor { get; set; }
+        private int Ammunition { get; set; }
+        private int Mobility { get; set; }
+        private int winPoints = 0;
+        private Random rand= new Random();
+
+       public Tank(string name)
+        {
+            rand.Next(100);
+            Name = name;
+            Armor=  rand.Next(100);
+            Ammunition = rand.Next(100);
+            Mobility = rand.Next(100);
+            
+        }
+
+        public static Tank operator ^(Tank t34, Tank pantera)
+        {
+            if (t34.Armor > pantera.Armor) t34.winPoints += 1;
+            else if (t34.Armor < pantera.Armor) pantera.winPoints += 1;
+
+
+            if (t34.Ammunition > pantera.Ammunition) t34.winPoints += 1;
+            else if (t34.Ammunition < pantera.Ammunition) pantera.winPoints += 1;
+
+            if (t34.Mobility > pantera.Mobility) t34.winPoints += 1;
+            else if (t34.Mobility < pantera.Mobility) pantera.winPoints += 1;
+
+            if (t34.winPoints > pantera.winPoints) return t34;
+            else if (t34.winPoints < pantera.winPoints)return pantera;
+
+            return t34;
+        }
+
+        public void GetInfo(Tank tank)
+        {
+            Console.WriteLine("Name: {0}",tank.Name);
+            Console.WriteLine("Modility: {0}", tank.Mobility);
+            Console.WriteLine("Ammunity: {0}",tank.Ammunition);
+            Console.WriteLine("Armor: {0}",tank.Armor);
+        }
+           
+    }
+}
